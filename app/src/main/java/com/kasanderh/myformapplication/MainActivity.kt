2eee7e8f188b32c93a1fc336ab562ea66d35aeeb
Kasanderh <Kasanderh@gmail.com>
2021-02-04 15:18:34 +0100

    Switched activity_summary to activity_main Added onClickListener for the submit button

--- app/src/main/java/com/kasanderh/myformapplication/MainActivity.kt
+++ app/src/main/java/com/kasanderh/myformapplication/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.kasanderh.myformapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        button_submit.setOnClickListener {
+            onSubmitClick()
+        }
+
+        val spinnerValues: Array<String> = arrayOf("Android Developer", "Android Engineer")
+        val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, spinnerValues)
+        spinner_job_title.adapter = spinnerAdapter
+    }
+
+    private fun onSubmitClick() {
+
+        val formData = FormData(
+                spinner_job_title.selectedItem?.toString(),
+                edit_text_contact_name_first.text.toString(),
+                edit_text_contact_name_last.text.toString(),
+                edit_text_contact_email.text.toString(),
+                edit_text_phone_number.text.toString(),
+                edit_text_user_password.text.toString()
+        )
+
+        val summaryActivityIntent = Intent(this, SummaryActivity::class.java)
+        summaryActivityIntent.putExtra("FormData", formData)
+        startActivity(summaryActivityIntent)
+
     }
 }
