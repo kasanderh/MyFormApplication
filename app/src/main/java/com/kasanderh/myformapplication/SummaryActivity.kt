239d6d0997001900916491ee12fc7718503f9527
Kasander Hanssen <kasanderh@gmail.com>
2021-02-05 16:38:29 +0100

    Put all of summary data in one field instead of individual. AutoLink set to All

--- app/src/main/java/com/kasanderh/myformapplication/SummaryActivity.kt
+++ app/src/main/java/com/kasanderh/myformapplication/SummaryActivity.kt
@@ -7,10 +7,11 @@ import kotlinx.android.synthetic.main.activity_summary.*
 class SummaryActivity : AppCompatActivity() {
 
     private lateinit var formData: FormData
-    private lateinit var summaryTextName: String
-    private lateinit var summaryTextEmail: String
-    private lateinit var summaryTextNumber: String
-    private lateinit var summaryTextTitle: String
+    private lateinit var summaryText: String
+//    private lateinit var summaryTextName: String
+//    private lateinit var summaryTextEmail: String
+//    private lateinit var summaryTextNumber: String
+//    private lateinit var summaryTextTitle: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,31 +24,50 @@ class SummaryActivity : AppCompatActivity() {
 
     private fun displaySummary() {
         formData = intent.getSerializableExtra("FormData") as FormData
-        summaryTextName = """
-            Contact Name: 
-            ${formData.contactNameFirst} ${formData.contactNameLast}
-        """.trimIndent()
-        text_view_name.text = summaryTextName
 
-        summaryTextEmail = """
-            Contact Email:
-            ${formData.contactEmail}
-        """.trimIndent()
-        text_view_email.text = summaryTextEmail
-
-        summaryTextNumber = """
-            Contact Number:
-            ${formData.contactNumber}
-        """.trimIndent()
-        text_view_number.text = summaryTextNumber
-
-        summaryTextTitle = """
+        summaryText = """
             Title:
             ${formData.jobTitle}
+            
+            Contact Name: 
+            ${formData.contactNameFirst} ${formData.contactNameLast}
+            
+            Contact Email:
+            ${formData.contactEmail}
+            
+            Contact Number:
+            ${formData.contactNumber}
+            
+            ${formData.passwordStrength()}            
         """.trimIndent()
-        text_view_title.text = summaryTextTitle
 
-        text_view_password.text = formData.passwordStrength()
+        text_view_name.text = summaryText
+
+//        summaryTextName = """
+//            Contact Name:
+//            ${formData.contactNameFirst} ${formData.contactNameLast}
+//        """.trimIndent()
+////        text_view_name.text = summaryTextName
+//
+//        summaryTextEmail = """
+//            Contact Email:
+//            ${formData.contactEmail}
+//        """.trimIndent()
+////        text_view_email.text = summaryTextEmail
+//
+//        summaryTextNumber = """
+//            Contact Number:
+//            ${formData.contactNumber}
+//        """.trimIndent()
+////        text_view_number.text = summaryTextNumber
+//
+//        summaryTextTitle = """
+//            Title:
+//            ${formData.jobTitle}
+//        """.trimIndent()
+////        text_view_title.text = summaryTextTitle
+////
+////        text_view_password.text = formData.passwordStrength()
 
     }
 }
